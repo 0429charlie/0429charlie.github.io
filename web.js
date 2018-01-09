976333df50153082cae41f6151bf43ac978c49e3
0429charlie <0429charlie@gmail.com>
2018-01-09 12:18:59 -0500

    update1
    Some update to teh website

--- web.js
+++ web.js
@@ -1,3 +1,3 @@
 function Loading() {
-    alert("Page is still under construction");
+    alert("Page is still under construction. It is partially done. Please ask me any additional questions about content that is not done yet (noted as 'coming soon' on the page)");
 }
