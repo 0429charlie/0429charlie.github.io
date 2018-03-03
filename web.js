8b930fb85cdfeffd80b4c837ae623ff5b3f5af1f
0429charlie <0429charlie@gmail.com>
2018-03-03 15:11:59 -0500

    Co-op and project
    Explanation updated

--- web.js
+++ web.js
@@ -1,3 +1,3 @@
 function Loading() {
-    alert("If interested in any project listed here, please hover over the project and click to see. Otherwise, please contact me for more detail");
+    alert("The title of each paragraph is a co-op position with the organization name. Many of them are a link to the company's website. Please click to visit the website of the company that I had work for. The subtitle of each paragraph is the project that I had work on. Some of them are also link to the end-product that I am allow to show it (permission granted from the company or school). Otherwise, I have the source code prototype that I developed and I am able show them in person during interview.");
 }
